9c3be077b221f723de11d5ff14d0c5b01011be75
Krishna Tandon <krishnatandon.1208@gmail.com>
2021-01-24 10:41:27 +0530

    about section updates

--- src/section/About.js
+++ src/section/About.js
@@ -11,14 +11,26 @@ const useStyles = makeStyles({
 		padding: '100px 0'
 	},
 	abouttext: siteHeadingTitle,
+	aboutdetails: {
+		margin: '70px auto 0',
+		display: 'flex',
+		justifyContent: 'center',
+		color: `${blue}`,
+		'& > Typography': {
+			fontWeight: 'bold'
+		}
+	},
 	ktImg: {
-		width: '500px',
-		height: '500px',
+		width: '350px',
+		height: '350px',
 		boxShadow: `20px 20px 0 0 ${yellow}, -20px -20px 0 0 ${blue}`,
 		transition: '0.5s',
 		'&:hover': {
 			boxShadow: `-20px -20px 0 0 ${yellow}, 20px 20px 0 0 ${blue}`
-		}
+		},
+		margin: '70px auto 0',
+		display: 'flex',
+		justifyContent: 'center'
 	}
 });
 
@@ -29,16 +41,20 @@ export default function About() {
 		<div className={classes.aboutSection}>
 			<Container>
 				<Grid container>
-					<Grid item xs={6} className={classes.abouttext}>
+					<Grid item xs={12} className={classes.abouttext}>
 						<Typography variant={'h4'} align="center">
 							About
 						</Typography>
 					</Grid>
-					<Grid item xs={false} lgUp={6}>
-						<Box display="flex" justifyContent="center">
-							<img src={KrishnaTandon} className={classes.ktImg} alt="Krishna Tandon" />
-						</Box>
+					<Grid item xs={6} className={classes.aboutdetails}>
+						<Typography variant={'h5'} align="center" fontWeight="fontWeightBold">
+							Hi, I am Krishna. Web developer with 5+ years of experience. Currently, I am UI Technical
+							Lead at TCS, India.
+						</Typography>
 					</Grid>
+					<Box component={Grid} md={6} display={{ xs: 'none', md: 'block' }}>
+						<img src={KrishnaTandon} className={classes.ktImg} alt="Krishna Tandon" />
+					</Box>
 				</Grid>
 			</Container>
 		</div>
